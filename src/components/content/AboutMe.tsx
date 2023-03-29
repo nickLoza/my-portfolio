57fa6299328a1a02d9038333d2078046b7b4321f
Nick Loza <nicklozadeveloper@gmail.com>
2023-03-29 19:58:04 -0300

    add: update icon

--- src/components/content/AboutMe.tsx
+++ src/components/content/AboutMe.tsx
@@ -9,7 +9,7 @@ function AboutMe(){
 			<img src="/assets/images/profile.png" className="about__img" alt="profile"/>
 			<div className="about__text">
 				<h2 className="about__text-title">I'm Nick Loza</h2>
-				<p className="about__text-description">I'm a front-end developer with a <b className="about__text-b">constant</b> interest in writing good code, learning new tecnologies and techniques to <b className="about__text-b">mutate</b> into a better programmer.
+				<p className="about__text-description">I'm a front-end developer with a <b className="about__text-b">constant</b> interest in writing good code, learning new technologies and techniques to <b className="about__text-b">mutate</b> into a better programmer.
 		    	</p>
 			</div>
 		</section>
