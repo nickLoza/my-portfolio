ba66ac525a658ab402d1b54a03fac792a342a190
Nick Loza <nicklozadeveloper@gmail.com>
2023-06-18 15:21:18 -0300

    add: paypal clone

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -7,57 +7,59 @@ import it from "../../assets/it.svg"
 
 function Aside() {
 	return (
-		<aside className='fixed left-0 top-0 md:w-[300px] w-[150px] h-full bg-[#1D2939] text-white text-center p-[1rem] border-r-[1px]'>
+		<aside className='w-[70px] fixed left-0 top-0 lg:w-[250px] h-full bg-[#1D2939] text-white text-center p-[1rem] border-r-[1px]'>
 			{/*top*/}
 			<div>
-				<p className='text-3xl font-bold mb-2'>Nick Loza</p>
-				<img className='h-[150px] rounded-[18px] m-auto' src={Profile} alt="Nick Loza" height={100}/>
-				<p className='text-2xl text-[#ccc] mb-3'>Frontend Developer</p>
-				<b className='text-xl'>My Lenguages:</b>
-				<div className='flex justify-center gap-5 mt-2'>
-					<span role="img" aria-label="Spanish">
-        				<img src={es} alt='spanish' width={25}/>
-				    </span>
-				    <span role="img" aria-label="English">
-				        <img src={gb} alt='english' width={25}/>
-				    </span>
-				    <span role="img" aria-label="Italian">
-				    	<img src={it} alt='italian' width={25}/>
-				    </span>
+				<div className='sm:hidden lg:block'>	
+					<p className='text-3xl font-bold mb-2'>Nick Loza</p>
+					<img className='h-[150px] rounded-[18px] m-auto' src={Profile} alt="Nick Loza" height={100}/>
+					<p className='text-2xl text-[#ccc] mb-3'>Frontend Developer</p>
+					<b className='text-xl'>My Lenguages:</b>
+					<div className='flex justify-center gap-5 mt-2'>
+						<span role="img" aria-label="Spanish">
+	        				<img src={es} alt='spanish' width={25}/>
+					    </span>
+					    <span role="img" aria-label="English">
+					        <img src={gb} alt='english' width={25}/>
+					    </span>
+					    <span role="img" aria-label="Italian">
+					    	<img src={it} alt='italian' width={25}/>
+					    </span>
 				</div>
-				<ul className='flex flex-col items-center justify-center mt-12 mb-8 pl-5'>
+				</div>	
+				<ul className='flex flex-col justify-center mt-12 mb-8 pl-1 lg:pl-5'>
 			      <li className='transition-colors ease-in-out delay-50 hover:text-[#B58956] mb-2'>
-			        <a className='flex items-center w-[150px] text-xl' href="https://www.linkedin.com/in/nicklozadev/" target='_blank'>
-			          <FaLinkedin  className="w-5 mr-2" /> LinkedIn
+			        <a className='flex items-center w-[150px] text-4xl lg:text-xl' href="https://www.linkedin.com/in/nicklozadev/" target='_blank'>
+			          <FaLinkedin  className="w-5 mr-2" /> <span className='hidden lg:block'>LinkedIn</span>
 			        </a>
 			      </li>
 			      <li className='transition-colors ease-in-out delay-50 hover:text-[#B58956] mb-2'>
-			        <a className='flex items-center w-[150px] text-xl' href="https://github.com/nickLoza" target='_blank'>
-			          <FaGithub className="w-5 mr-2" /> GitHub
+			        <a className='flex items-center w-[150px] text-4xl lg:text-xl' href="https://github.com/nickLoza" target='_blank'>
+			          <FaGithub className="w-5 mr-2" /> <span className='hidden lg:block'>GitHub</span>
 			        </a>
 			      </li>
 			      <li className='transition-colors ease-in-out delay-50 hover:text-[#B58956] mb-2'>
-			        <a className='flex items-center w-[150px] text-xl' href="mailto:nicklozadeveloper@gmail.com" target='_blank'>
-			          <FaEnvelope className="w-5 mr-2" /> Gmail
+			        <a className='flex items-center w-[150px] text-4xl lg:text-xl' href="mailto:nicklozadeveloper@gmail.com" target='_blank'>
+			          <FaEnvelope className="w-5 mr-2" /> <span className='hidden lg:block'>Gmail</span>
 			        </a>
 			      </li>
 			    </ul>
 			</div>
 			{/*bottom*/}
-			<ul className='flex flex-col items-center justify-center my-4 pl-4'>
+			<ul className='flex flex-col justify-center my-4 pl-1 lg:pl-4'>
 					<li className='transition-colors ease-in-out delay-50 hover:text-[#B58956] mb-2'>
-				        <a className='flex items-center w-[150px] text-xl' href="#about">
-				          	<FaUser className="w-5 mr-2"/> About Me
+				        <a className='flex items-center w-[150px] text-4xl lg:text-xl' href="#about">
+				          	<FaUser className="w-5 mr-2"/> <span className='hidden lg:block'>About Me</span>
 				        </a>
 			      	</li>
 					<li className='transition-colors ease-in-out delay-50 hover:text-[#B58956] mb-2'>
-				        <a className='flex items-center w-[150px] text-xl' href="#projects">
-				          	<FaDesktop className="w-5 mr-2"/> Projects
+				        <a className='flex items-center w-[150px] text-4xl lg:text-xl' href="#projects">
+				          	<FaDesktop className="w-5 mr-2"/> <span className='hidden lg:block'>Projects</span>
 				        </a>
 			      	</li>
 			      	<li className='transition-colors ease-in-out delay-50 hover:text-[#B58956] mb-2'>
-				        <a className='flex items-center w-[150px] text-xl' href="https://drive.google.com/file/d/15QWOvEur3FUzgjGaSgoKpsfcWbfCqJC-/view?usp=sharing" target='_blank'>
-				          	<FaFile className="w-5 mr-2"/> Resume
+				        <a className='flex items-center w-[150px] text-4xl lg:text-xl' href="https://drive.google.com/file/d/15QWOvEur3FUzgjGaSgoKpsfcWbfCqJC-/view?usp=sharing" target='_blank'>
+				          	<FaFile className="w-5 mr-2"/> <span className='hidden lg:block'>Resume</span>
 				        </a>
 			      	</li>
 				</ul>
--- src/projectsDB/projectsDB.ts
+++ src/projectsDB/projectsDB.ts
@@ -4,11 +4,11 @@
 const projectsDB = [
 	{
 		id: 1, 
-		imgUrl: "https://i.postimg.cc/NMhqQDq0/clothes-shop-preview.webp", 
-		title: "E-commerce clothing",
-		websiteLink: "https://clothes-shop-react-web.netlify.app/home", 
-		repositoryLink: "https://github.com/nickLoza/e-commerce-clothes",
-		description: "React-TS e-commerce with Redux Toolkit and Swiper.js"},
+		imgUrl: "https://i.postimg.cc/0Q5tKZNn/preview1200.jpg", 
+		title: "Spotify Clone",
+		websiteLink: "https://spotify-clone-react-nickloza.netlify.app/", 
+		repositoryLink: "https://github.com/nickLoza/spotify-clone-react",
+		description: "React-TS and tailwind"},
 	{
 		id: 2, 
 		imgUrl: "https://i.postimg.cc/kXM6X9PM/twitch-clone-preview.webp", 
@@ -25,11 +25,11 @@ const projectsDB = [
 		description: "Video chat app with Next.js, and a server with express and socket.io"},
 	{
 		id: 4, 
-		imgUrl: "https://i.postimg.cc/zX6SP5GH/band-preview-portfolio.webp", 
-		title: "A Band Website",
-		websiteLink: "https://band-website-react.netlify.app/home", 
-		repositoryLink: "https://github.com/nickLoza/band-website",
-		description: "React-TS and SASS"},
+		imgUrl: "https://i.postimg.cc/W1Gyj736/paypal-preview.jpg", 
+		title: "PayPal Clone",
+		websiteLink: "https://clone-react-tailwind.netlify.app/", 
+		repositoryLink: "https://github.com/nickLoza/paypal-clone-react-tailwind",
+		description: "React, Typescript, and Tailwind"},
 	{
 		id: 5, 
 		imgUrl: "https://i.postimg.cc/2yXt1pcj/motorcycles-preview.webp", 
@@ -37,20 +37,27 @@ const projectsDB = [
 		websiteLink: "https://electric-motorcycles-react.netlify.app/", 
 		repositoryLink: "https://github.com/nickLoza/motorcycle-company",
 		description: "React-TS, SASS, context provider"},
-		{
+	{
 		id: 6, 
-		imgUrl: "https://i.postimg.cc/4dmSTs4V/chat-app-prev.webp", 
-		title: "Chat App",
-		websiteLink: "https://chat-app-world.netlify.app/", 
-		repositoryLink: "https://github.com/nickLoza/chat-app-world",
-		description: "React-TS, SASS, Webpack, Babel and firebase"},
+		imgUrl: "https://i.postimg.cc/zX6SP5GH/band-preview-portfolio.webp", 
+		title: "A Band Website",
+		websiteLink: "https://band-website-react.netlify.app/home", 
+		repositoryLink: "https://github.com/nickLoza/band-website",
+		description: "React-TS and SASS"},
 	{
 		id: 7, 
 		imgUrl: "https://i.postimg.cc/66Hy4RfZ/img-optimizer-prev-port.webp", 
 		title: "Img resizer/to WEBP",
 		websiteLink: "https://img-resize-to-webp.netlify.app/", 
 		repositoryLink: "https://github.com/nickLoza/image-resizer-webp-converter",
-		description: "React-TS, drag & drop, useReducer, react-image-file-resizer"}
+		description: "React-TS, drag & drop, useReducer, react-image-file-resizer"},
+	{
+		id: 8, 
+		imgUrl: "https://i.postimg.cc/NMhqQDq0/clothes-shop-preview.webp", 
+		title: "E-commerce clothing",
+		websiteLink: "https://clothes-shop-react-web.netlify.app/home", 
+		repositoryLink: "https://github.com/nickLoza/e-commerce-clothes",
+		description: "React-TS e-commerce with Redux Toolkit and Swiper.js"},
 ];
 
 
